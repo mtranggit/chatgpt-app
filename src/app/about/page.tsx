a1a20124f115be1147070a6faffbc018c57a20db
Michael Trang <michael.trang@hotmail.com>
2024-11-29 12:55:37 +0000

    Update layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">About Us</h1>
+			<p>About us and our Awesome Application</p>
+    </main>
+  );
+}
